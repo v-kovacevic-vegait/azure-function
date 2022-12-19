a71f8ca956b5fe53a276b17c4755fe402321f0bf
v-kovacevic-vegait <v.kovacevic@vegait.rs>
2022-12-19 12:37:07 +0100

    change

--- HttpTrigger1.cs
+++ HttpTrigger1.cs
@@ -27,7 +27,7 @@ namespace azurefunction
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully. Yeeees!";
+                : $"Hello, {name}. This HTTP triggered function executed successfully. Yeeees! :D";
 
             return new OkObjectResult(responseMessage);
         }
